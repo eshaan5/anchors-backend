d8a14ab8c09d7fae8a643117595f7ee1729417d6
eshaan <eshaanbagga@gmail.com>
2023-12-09 16:43:13 +0530

    done with videos

--- controllers/video.js
+++ controllers/video.js
@@ -2,26 +2,39 @@ import Video from "../models/video.js";
 
 // Create a new video
 export const createVideo = (req, res) => {
-    // Create a new video using the Video mongoose model
-    const newVideo = new Video(req.body);
+    // Extract the video title from the request
+    const videoTitle = req.body.title;
 
-    // Save it to database
-    newVideo.save((err, video) => {
-        if (err) {
-            res.send(err);
-        }
-        console.log(video);
-        res.json(video);
-    });
+    // Try to find a video with the same title in the database
+    Video.findOne({ title: videoTitle })
+        .then(existingVideo => {
+            // If a video with the same title is found, update its content
+            if (existingVideo) {
+                existingVideo.set(req.body); // Replace the existing video content
+                return existingVideo.save();
+            } else {
+                // If no video with the same title is found, create a new one
+                const newVideo = new Video(req.body);
+                return newVideo.save();
+            }
+        })
+        .then(savedVideo => {
+            res.json(savedVideo);
+        })
+        .catch(err => {
+            res.status(500).json({ error: err.message });
+        });
 };
 
 // Get all videos
 export const getVideos = (req, res) => {
     // Find all videos and sort by 'earnings'
-    Video.find({}).sort({ earnings: -1 }).exec((err, videos) => {
-        if (err) {
+    Video.find({}).sort({ earnings: -1 }).exec()
+        .then((videos) => {
+            console.log(videos);
+            res.json(videos);
+        })
+        .catch((err) => {
             res.send(err);
-        }
-        res.json(videos);
-    });
+        });
 };
